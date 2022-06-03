39d405a6c5bffded4efb44a1a6c9f353bcffdd88
Paulito <subhero007@hotmail.com>
2022-06-03 00:30:06 -0600

    extended model + UI

--- services/crawler/project/models.py
+++ services/crawler/project/models.py
@@ -7,16 +7,57 @@ from database import engine
 Base = declarative_base()
 
 
-class AccountMetric(Base):
-    __tablename__ = "accountmetric"
+class AccountStat(Base):
+    __tablename__ = "accountstat"
 
     id = Column(Integer, primary_key=True)
     address = Column(String(100), nullable=False, unique=True)
     name = Column(String(100), nullable=False, unique=True)
-    balance = Column(Integer)
-    towerheight = Column(Integer)
+    balance = Column(Integer, nullable=False, default=0)
+    towerheight = Column(Integer, nullable=False, default=0)
+    proofsinepoch = Column(Integer, nullable=False, default=0)
+    lastepochmined = Column(Integer, nullable=False, default=0)
     created_at = Column(DateTime, server_default=func.now())
     updated_at = Column(DateTime, server_default=func.now())
 
+
+class MinerHistory(Base):
+    __tablename__ = "minerhistory"
+
+    id = Column(Integer, primary_key=True)
+    address = Column(String(100), nullable=False, unique=True)
+    epoch = Column(Integer, nullable=False, default=0)
+    proofssubmitted = Column(Integer, nullable=False, default=0)
+    created_at = Column(DateTime, server_default=func.now())
+    updated_at = Column(DateTime, server_default=func.now())
+
+
+class PaymentEvent(Base):
+    __tablename__ = "paymentevent"
+
+    id = Column(Integer, primary_key=True)
+    address = Column(String(100), nullable=False, unique=True)
+    height = Column(Integer, nullable=False, default=0)
+    type = Column(String(100), nullable=False)
+    amount = Column(Integer, nullable=False, default=0)
+    sender = Column(String(100))
+    recipient = Column(String(100))
+    created_at = Column(DateTime, server_default=func.now())
+    updated_at = Column(DateTime, server_default=func.now())
+
+
+class ChainEvent(Base):
+    __tablename__ = "chainevent"
+
+    id = Column(Integer, primary_key=True)
+    address = Column(String(100), nullable=False, unique=True)
+    height = Column(Integer, nullable=False, default=0)
+    timestamp = Column(DateTime, nullable=False, default=func.now())
+    type = Column(String(100), nullable=False, default="")
+    status = Column(String(100), nullable=False, default="")
+    sender = Column(String(100))
+    recipient = Column(String(100))
+    created_at = Column(DateTime, server_default=func.now())
+    updated_at = Column(DateTime, server_default=func.now())
 
 # Base.metadata.create_all(engine)
--- services/web/project/main.py
+++ services/web/project/main.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
-from .models import AccountMetric
+from .models import AccountStat
 from . import db
 
 main = Blueprint('main', __name__)
@@ -14,15 +14,17 @@ def index():
 @main.route('/dashboard')
 @login_required
 def dashboard():
-    rows = AccountMetric.query.all()
-    totalbalance = db.session.query(db.func.sum(AccountMetric.balance)).scalar()
-    totalheight = db.session.query(db.func.sum(AccountMetric.towerheight)).scalar()
-    nrofaccounts = db.session.query(db.func.count(AccountMetric.address)).scalar()
-    lastupdate = db.session.query(db.func.max(AccountMetric.updated_at)).scalar()
+    rows = AccountStat.query.order_by(db.asc(AccountStat.name)).all()
+    totalbalance = db.session.query(db.func.sum(AccountStat.balance)).scalar()
+    totalheight = db.session.query(db.func.sum(AccountStat.towerheight)).scalar()
+    nrofaccounts = db.session.query(db.func.count(AccountStat.address)).scalar()
+    lastupdate = db.session.query(db.func.max(AccountStat.updated_at)).scalar()
+    lastepochmined = db.session.query(db.func.max(AccountStat.lastepochmined)).scalar()
     return render_template('dashboard.html',
                            name=current_user.name,
                            rows=rows,
                            totalbalance=totalbalance,
                            totalheight=totalheight,
                            lastupdate=lastupdate,
-                           nrofaccounts=nrofaccounts)
+                           nrofaccounts=nrofaccounts,
+                           lastepochmined=lastepochmined)
