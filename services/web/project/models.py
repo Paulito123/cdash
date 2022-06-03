cc8aa1753bb6d8fdc349ec1ea993386bd1998bce
Paulito <subhero007@hotmail.com>
2022-06-03 00:30:19 -0600

    extended model + UI

--- services/web/project/models.py
+++ services/web/project/models.py
@@ -7,7 +7,7 @@ class User(UserMixin, db.Model):
     __tablename__ = "user"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(200), nullable=False, unique=False)
+    name = db.Column(db.String(100), nullable=False, unique=False)
     email = db.Column(db.String(40), unique=True, nullable=False)
     password = db.Column(
         db.String(200), primary_key=False, unique=False, nullable=False
@@ -25,20 +25,58 @@ class User(UserMixin, db.Model):
         return "<User {}>".format(self.name)
 
 
-class AccountMetric(db.Model):
-    __tablename__ = "accountmetric"
+class AccountStat(db.Model):
+    __tablename__ = "accountstat"
 
     id = db.Column(db.Integer, primary_key=True)
     address = db.Column(db.String(100), nullable=False, unique=True)
     name = db.Column(db.String(100), nullable=False, unique=True)
-    balance = db.Column(db.Integer)
-    towerheight = db.Column(db.Integer)
+    balance = db.Column(db.Integer, nullable=False, default=0)
+    towerheight = db.Column(db.Integer, nullable=False, default=0)
+    proofsinepoch = db.Column(db.Integer, nullable=False, default=0)
+    lastepochmined = db.Column(db.Integer, nullable=False, default=0)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, server_default=db.func.now())
 
     def __repr__(self):
-        return "<tr><td>{}</td><td>{}</td><td>{}</td><td>{}</td></tr>".format(
-            self.address,
-            self.name,
-            self.balance,
-            self.towerheight)
+        return f'<a href="https://0l.interblockcha.in/address/{self.address}">{self.address}</a>'
+
+
+class MinerHistory(db.Model):
+    __tablename__ = "minerhistory"
+
+    id = db.Column(db.Integer, primary_key=True)
+    address = db.Column(db.String(100), nullable=False, unique=True)
+    epoch = db.Column(db.Integer, nullable=False, default=0)
+    proofssubmitted = db.Column(db.Integer, nullable=False, default=0)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now())
+
+
+class PaymentEvent(db.Model):
+    __tablename__ = "paymentevent"
+
+    id = db.Column(db.Integer, primary_key=True)
+    address = db.Column(db.String(100), nullable=False, unique=True)
+    height = db.Column(db.Integer, nullable=False, default=0)
+    type = db.Column(db.String(100), nullable=False)
+    amount = db.Column(db.Integer, nullable=False, default=0)
+    sender = db.Column(db.String(100))
+    recipient = db.Column(db.String(100))
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now())
+
+
+class ChainEvent(db.Model):
+    __tablename__ = "chainevent"
+
+    id = db.Column(db.Integer, primary_key=True)
+    address = db.Column(db.String(100), nullable=False, unique=True)
+    height = db.Column(db.Integer, nullable=False, default=0)
+    timestamp = db.Column(db.DateTime, nullable=False, default=db.func.now())
+    type = db.Column(db.String(100), nullable=False, default="")
+    status = db.Column(db.String(100), nullable=False, default="")
+    sender = db.Column(db.String(100))
+    recipient = db.Column(db.String(100))
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now())
