3f2ac379b28e212dae5e39c66d9cb86030f70c37
Paulito <subhero007@hotmail.com>
2022-06-03 18:33:41 -0600

    backup temp

--- services/crawler/project/app.py
+++ services/crawler/project/app.py
@@ -1,32 +1,88 @@
 import requests
-from lxml import html
 from time import sleep
 from sqlalchemy import text
 from sqlalchemy.exc import ProgrammingError
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
 from models import AccountStat
 from database import session, engine
 from config import Config
 from datetime import datetime
 
 
-def get_source(page_url):
-    """
-    A function to download the page source of the given URL.
-    """
-    r = requests.get(page_url)
-    source = html.fromstring(r.content)
+def fetch_0l_table_data(driver, xp_rows, xp_button_next, data_name_list):
+    print("Waiting for data...")
+    WebDriverWait(driver, timeout=30).until(lambda d: d.find_element(By.XPATH, xp_button_next))
 
-    return source
+    # Crawling miner history
+    disabled = False
+    while not disabled:
+
+        rows = driver.find_elements(By.XPATH, xp_rows)
+        for row in rows:
+            if len(row.find_elements(By.TAG_NAME, "td")[0].text) == 0:
+                print("skip empty")
+                continue
+
+            col_index = 0
+            for data_name in data_name_list:
+                v = row.find_elements(By.TAG_NAME, "td")[col_index]
+                print(f"{data_name} = {v.text}")
+                col_index = col_index + 1
+
+        button_next = driver.find_element(By.XPATH, xp_button_next)
+        disabled = button_next.get_property('disabled')
+        button_next.click()
 
 
 def scrape():
     with engine.connect() as connection:
         try:
+            # Fetch the account list
             accounts = connection.execute(text("select address from accountstat"))
 
+            # iterate account list
             for account in accounts:
-                www_page = Config.BASE_URL + 'address/' + account.address
-                source = get_source(www_page)
+                url = Config.BASE_URL + 'address/' + account.address
+                exec_host = 'chrome'
+
+                chrome_options = webdriver.ChromeOptions()
+                chrome_options.add_argument("start-maximized")
+                chrome_options.add_argument("disable-infobars")
+                chrome_options.add_argument("--disable-extensions")
+
+                driver = webdriver.Remote(
+                    command_executor=f'http://{exec_host}:4444/wd/hub',
+                    options=chrome_options
+                )
+
+                try:
+                    driver.get(url)
+
+                    address = driver.find_element(By.CLASS_NAME, "address_addressText__YS_A5")
+                    balance = driver.find_element(By.CLASS_NAME, "address_balanceText__ds0io")
+                    height = driver.find_element(By.XPATH,
+                                                 "//div[contains(@class, 'address_statsTablesContainer___HxvE')]/div[1]/div[2]/div/div/div/div/div/table/tbody/tr[1]/td[2]")
+                    proofsinepoch = driver.find_element(By.XPATH,
+                                                        "//div[contains(@class, 'address_statsTablesContainer___HxvE')]/div[1]/div[2]/div/div/div/div/div/table/tbody/tr[2]/td[2]")
+                    lastepochmined = driver.find_element(By.XPATH,
+                                                         "//div[contains(@class, 'address_statsTablesContainer___HxvE')]/div[1]/div[2]/div/div/div/div/div/table/tbody/tr[3]/td[2]")
+
+                    xp_rows = "//div[contains(@class, 'address_statsTablesContainer___HxvE')]/div[2]/div[2]/div/div/div/div/div/table/tbody/tr"
+                    xp_button_next = "//div[contains(@class, 'address_statsTablesContainer___HxvE')]/div[2]/div[2]/div/div/ul/li[@title='Next Page']/button"
+                    data_name_list = ["Epoch", "Proofs submitted"]
+                    fetch_0l_table_data(driver, xp_rows, xp_button_next, data_name_list)
+
+                    xp_rows = "//div[contains(@class, 'eventsTable_inner__HsGHV')]/div[2]/div/div/div/div/div/table/tbody/tr"
+                    xp_button_next = "//div[contains(@class, 'eventsTable_inner__HsGHV')]/div[2]/div/div/ul/li[@title='Next Page']/button"
+                    data_name_list = ["height", "type", "amount", "sender", "recipient"]
+                    fetch_0l_table_data(driver, xp_rows, xp_button_next, data_name_list)
+                except Exception as e:
+                    print(f"{e}")
+                finally:
+                    driver.quit()
+
 
                 address = source.xpath("//span[contains(@class, 'address_addressText__YS_A5')]/text()")
                 balance = source.xpath("//span[contains(@class, 'address_balanceText__ds0io')]/text()")
