b62752abbfd42ef0e30d66c79efd3ee2d092e417
Paulito <subhero007@hotmail.com>
2022-06-27 10:16:40 -0600

    notifier production ready

--- services/notifier/entrypoint.sh
+++ services/notifier/entrypoint.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
-if [ "$DATABASE" = "postgres" ]
-then
-    echo "Waiting for postgres..."
+echo "Waiting for postgres..."
 
-    while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 0.1
-    done
+while ! nc -z $SQL_HOST $SQL_PORT; do
+  sleep 0.1
+done
 
-    echo "PostgreSQL started"
-fi
+echo "PostgreSQL started"
 
 # Start cron
 echo "Wait for data: 10m"
