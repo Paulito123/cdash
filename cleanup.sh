0ff19678f9c22795743013e2df95c49341320a92
Paulito <subhero007@hotmail.com>
2022-07-29 14:57:31 -0600

    complete cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -8,8 +8,21 @@ rm -f .gitignore
 rm -f LICENSE
 rm -f README.md
 
+rm -f ./services/crawler/project/database/test.sql
+
+rm -f ./services/notifier/Dockerfile
+rm -f ./services/notifier/crontab.development
+rm -f ./services/notifier/project/database/test.sql
+
+rm -f ./services/web/Dockerfile
+rm -f ./services/web/entrypoint.sh
+rm -f ./services/web/secrets_dev.py
+rm -f ./services/web/project/test.sql
+
 rm -rf .git
 rm -rf .idea
+
+rm -f cleanup.sh
 
 echo "Ready to deploy..."
 
